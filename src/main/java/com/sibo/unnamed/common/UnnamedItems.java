911af4fcc07b248041da63175969a5e456c14cbd
azzerent <simpan.95@hotmail.com>
2017-05-03 16:44:47 +0200

    Test junction post
    junction

--- src/main/java/com/sibo/unnamed/common/UnnamedItems.java
+++ src/main/java/com/sibo/unnamed/common/UnnamedItems.java
@@ -9,10 +9,12 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class UnnamedItems {
 
 	public static ItemBase ingotCopper;
+	public static ItemBase ingotCopper2;
 	
 	
 	public static void init(){
 		ingotCopper = register(new ItemBase("ingotCopper").setCreativeTab(CreativeTabs.MATERIALS));
+		ingotCopper2 = register(new ItemBase("ingotCopper2").setCreativeTab(CreativeTabs.MATERIALS));
 	}
 	
 	private static <T extends Item> T register(T item){
